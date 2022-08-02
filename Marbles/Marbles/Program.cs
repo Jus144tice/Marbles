3d653fded84a86b6d445acaaec82bc62aff1a60c
Stephen Graham <jus144tice@gmail.com>
2022-08-02 11:26:41 -0400

    Played around with primitive types

--- Marbles/Marbles/Program.cs
+++ Marbles/Marbles/Program.cs
@@ -1,3 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello there");
+﻿string stringVariable = "Hello world";
+double doubleVariable = 123.456;
+bool boolVariable = true;
 
+int intVariable;
+
+
+Console.WriteLine("Enter an integer:");
+intVariable = int.Parse(Console.ReadLine());
+
+if (intVariable > 0)
+{
+    Console.WriteLine("It's positive");
+}
+else if (intVariable < 0)
+{
+    Console.WriteLine("It's negative");
+}
+else
+{
+    Console.WriteLine("It's exactly 0");
+}
+
+char charVariable;
+
+Console.WriteLine("Enter a character: ");
+charVariable = char.Parse(Console.ReadLine());
+Console.WriteLine("You entered: " + charVariable);
+
+int counter = 10;
+
+while (counter > 0)
+{
+    Console.WriteLine("Counter is now: " + counter);
+    --counter;
+
+    if (counter == 5)
+    {
+        Console.WriteLine("Ending the loop early");
+        break;
+    }
+}
+
+/*
+stringVariable = "Goodbye";
+intVariable = 5678;
+
+
+Console.WriteLine(intVariable);
+*/
